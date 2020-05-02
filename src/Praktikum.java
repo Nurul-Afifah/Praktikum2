158820b30128f5904cc9c33e08173b91a9d21147
NurulAfifah <nurullafifahh1112@gmail.com>
2020-05-02 09:27:10 +0700

    Initial commit

---
+++ src/Praktikum.java
@@ -0,0 +1,18 @@
+public class Praktikum {
+    String haridanJam;
+    String tanggal;
+    String pertemuan;
+    String kelas;
+    String dosenPengajar;
+
+    void cetakInfo(){
+        System.out.println("Hari & Jam : " + haridanJam);
+        System.out.println("Tanggal : " + tanggal);
+        System.out.println("Pertemuan : " + pertemuan);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("Dosen Pengajar : " + dosenPengajar);
+    }
+
+    void jadwalPraktikum(){
+    }
+}
